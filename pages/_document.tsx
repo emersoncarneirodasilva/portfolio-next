dfe6b545f5c2d722ca701aada44d06089145f6bf
Emerson Carneiro da Silva <94311606+emersoncarneirodasilva@users.noreply.github.com>
2022-05-19 12:48:24 -0300

    correcao font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Link from 'next/link';
 
 export default function Document() {
   return (
     <Html lang="pt-br">
       <Head>
         <title>Portifólio</title>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet" />
+        <Link rel="preconnect" href="https://fonts.gstatic.com" />
+        <Link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet" />
       </Head>
       <body className="bg-fixed bg-gradient-to-r from-green to-blue-500 dark:from-dark-300 dark:to-dark-700 dark:text-white">
         <Main />
