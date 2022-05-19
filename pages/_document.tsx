cb35b4990cbefb2a234a21bcf4b7c45ecf3b9782
Emerson Carneiro da Silva <94311606+emersoncarneirodasilva@users.noreply.github.com>
2022-05-19 12:42:28 -0300

    correcao font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
     <Html lang="pt-br">
       <Head>
         <title>Portifólio</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet" />
       </Head>
