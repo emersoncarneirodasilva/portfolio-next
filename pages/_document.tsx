5a686fe255d863f287da3521a1ec0cc177aea39f
Emerson Carneiro da Silva <94311606+emersoncarneirodasilva@users.noreply.github.com>
2022-05-19 13:21:30 -0300

    correcao font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,8 +6,7 @@ export default function Document() {
       <Head>
         <title>Portifólio</title>
         
-        <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet" />
-        
+                
       </Head>
       <body className="bg-fixed bg-gradient-to-r from-green to-blue-500 dark:from-dark-300 dark:to-dark-700 dark:text-white">
         <Main />
